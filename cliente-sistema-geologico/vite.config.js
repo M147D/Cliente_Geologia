349a06a8dc60f5933b395c6c945e594fa6d712eb
Miguel Pastuña <david.rmig@gmail.com>
2024-12-21 21:04:21 -0500

    Create proxy for dev

--- cliente-sistema-geologico/vite.config.js
+++ cliente-sistema-geologico/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://localhost:44371', // URL de tu backend
+        changeOrigin: true,
+        secure: false, // Si usas HTTPS con un certificado autofirmado
+      },
+    },
+  },
 })
